bdb1d64af3d18a52c513a8a92211b050e0c59ae0
Taha Samad <taha.samad@ebricks-inc.com>
2019-05-29 09:00:21 +0500

    Importing reset location

--- WebDriverAgentLib/Commands/FBFindElementCommands.m
+++ WebDriverAgentLib/Commands/FBFindElementCommands.m
@@ -155,5 +155,32 @@ static id<FBResponsePayload> FBNoSuchElementErrorResponseForRequest(FBRouteReque
   return elements;
 }
 
++ (NSArray *)elementsUsing:(NSString *)usingText withValue:(NSString *)value under:(XCUIElement *)element shouldReturnAfterFirstMatch:(BOOL)shouldReturnAfterFirstMatch
+{
+  NSArray *elements;
+  const BOOL partialSearch = [usingText isEqualToString:@"partial link text"];
+  const BOOL isSearchByIdentifier = ([usingText isEqualToString:@"name"] || [usingText isEqualToString:@"id"] || [usingText isEqualToString:@"accessibility id"]);
+  if (partialSearch || [usingText isEqualToString:@"link text"]) {
+    NSArray *components = [value componentsSeparatedByString:@"="];
+    NSString *propertyValue = components.lastObject;
+    NSString *propertyName = (components.count < 2 ? @"name" : components.firstObject);
+    elements = [element fb_descendantsMatchingProperty:propertyName value:propertyValue partialSearch:partialSearch];
+  } else if ([usingText isEqualToString:@"class name"]) {
+    elements = [element fb_descendantsMatchingClassName:value shouldReturnAfterFirstMatch:shouldReturnAfterFirstMatch];
+  } else if ([usingText isEqualToString:@"class chain"]) {
+    elements = [element fb_descendantsMatchingClassChain:value shouldReturnAfterFirstMatch:shouldReturnAfterFirstMatch];
+  } else if ([usingText isEqualToString:@"xpath"]) {
+    elements = [element fb_descendantsMatchingXPathQuery:value shouldReturnAfterFirstMatch:shouldReturnAfterFirstMatch];
+  } else if ([usingText isEqualToString:@"predicate string"]) {
+    NSPredicate *predicate = [FBPredicate predicateWithFormat:value];
+    elements = [element fb_descendantsMatchingPredicate:predicate shouldReturnAfterFirstMatch:shouldReturnAfterFirstMatch];
+  } else if (isSearchByIdentifier) {
+    elements = [element fb_descendantsMatchingIdentifier:value shouldReturnAfterFirstMatch:shouldReturnAfterFirstMatch];
+  } else {
+    [[NSException exceptionWithName:FBElementAttributeUnknownException reason:[NSString stringWithFormat:@"Invalid locator requested: %@", usingText] userInfo:nil] raise];
+  }
+  return elements;
+}
+
 @end
 
