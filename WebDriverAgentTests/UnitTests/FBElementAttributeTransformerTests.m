ffd66d9ae580c18b6efb01b54ea7f72f027d5b12
Taha Samad <taha.samad@ebricks-inc.com>
2019-11-07 17:07:11 +0500

    APPSIGHT-10518 :: Updating scheme to remove test case from runner.

--- WebDriverAgentTests/UnitTests/FBElementAttributeTransformerTests.m
+++ WebDriverAgentTests/UnitTests/FBElementAttributeTransformerTests.m
@@ -23,11 +23,16 @@
 }
 
 - (void)testDescription {
-  NSString *desc = @"Cell, 0x6000035d7e20, {{336.0, 212.0}, {296.0, 165.0}}, identifier: 'SBAUICarouselCell-1'\nOther, 0x6000035d7d40, {{336.0, 212.0}, {296.0, 165.0}}\nButton, 0x6000035d7c60, {{336.0, 212.0}, {296.0, 165.0}}, identifier: '/Users/taha.samad/Library/Developer/CoreSimulator/Devices/FDA7FE17-4847-4291-93F3-5A65121A6DFC/data/Containers/Data/Application/29CF97FC-AC46-45D4-AA23-0DB9A803CE42/Library/Caches/Images/-7757036882996523168.png', label: 'Each visit gets you closer to free food and drinks'";
+  
+  //This runs only on Xcode 10.x Simulators.
+  NSLog(@"Testing setShouldAnonymizeFullImagePaths");
+  
+  NSString *desc = @"Cell, 0x6000035d7e20, {{336.0, 212.0}, {296.0, 165.0}}, identifier: 'SBAUICarouselCell-1'\nOther, 0x6000035d7d40, {{336.0, 212.0}, {296.0, 165.0}}\nButton, 0x6000035d7c60, {{336.0, 212.0}, {296.0, 165.0}}, identifier: '/Users/JohnDoe/Library/Developer/CoreSimulator/Devices/SOMEDEVICE-4847-4291-93F3-5A65121A6DFC/data/Containers/Data/Application/SOMEAPP-AC46-45D4-AA23-0DB9A803CE42/Library/Caches/Images/SomeImage.png', label: 'This is a Test'";
   NSString *updatedDesc = [FBElementAttributeTransformer anonymizeValuesInDescription:desc];
   
-  NSLog(@"Original Desc:\n%@", desc);
-  NSLog(@"Updated Desc:\n%@", updatedDesc);
+  NSString *expectedDesc = @"Cell, 0x6000035d7e20, {{336.0, 212.0}, {296.0, 165.0}}, identifier: 'SBAUICarouselCell-1'\nOther, 0x6000035d7d40, {{336.0, 212.0}, {296.0, 165.0}}\nButton, 0x6000035d7c60, {{336.0, 212.0}, {296.0, 165.0}}, identifier: 'mesmer_anonymized_img.png', label: 'This is a Test'";
+  
+  XCTAssertTrue([updatedDesc isEqualToString:expectedDesc]);
 }
 
 @end
